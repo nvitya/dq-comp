0b292e2b5e92917a77f6ead3b20a50bdf9dd5632
Viktor Nagy <nvitya@gmail.com>
2026-03-10 06:23:52 +0100

    struct test created.

---
+++ tests/basic/struct/tb_struct.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+extern int dq_struct_test();
+
+int main() {
+    printf("Running struct test...\n");
+    dq_struct_test();
+    printf("Struct test finished.\n");
+    return 0;
+}
