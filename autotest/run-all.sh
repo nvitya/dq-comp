bf7eea54664d42128b6fd5817ce480bc5bbe6fd9
Viktor Nagy <nvitya@gmail.com>
2026-04-06 00:01:17 +0200

    updated autotest/run-all.sh

--- autotest/run-all.sh
+++ autotest/run-all.sh
@@ -6,6 +6,6 @@ SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 REPO_DIR=$(CDPATH= cd -- "$SCRIPT_DIR/.." && pwd)
 
 "$REPO_DIR/autotest/atrunner/build/dqatrun" \
-  -c "$REPO_DIR/compiler/build/dq-comp" \
+  -c "$REPO_DIR/build/dq-comp" \
   -r "$REPO_DIR/autotest/tests"
 
